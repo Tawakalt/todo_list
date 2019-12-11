df91cef4033b3da256dbcf749523b89706ae0b57
Tawakalt <olaniyitaiwo95@yahoo.com>
2019-12-11 21:41:21 +0100

    refactor html to pass some assertions

--- functional_tests.py
+++ functional_tests.py
@@ -34,13 +34,14 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        input_box.send_keys(Keys.Enter)
+        input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.Text == '1: Buy peacock feathers' for row in rows)
+            any(row.Text == '1: Buy peacock feathers' for row in rows),
+            'New to-do item did not appear in table'
         )
 
         # There is still a text box inviting her to add another item. She
