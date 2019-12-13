23d445b7c7256e2c95848459ecb5c62bb27f340e
Tawakalt <olaniyitaiwo95@yahoo.com>
2019-12-13 14:15:31 +0100

    change url

--- lists/tests.py
+++ lists/tests.py
@@ -5,27 +5,27 @@ from .models import Item
 class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
-        response = self.client.get('/lists/')
+        response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/lists/', data={'item_text':'A new list item'})
+        self.client.post('/', data={'item_text':'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/', data={'item_text':'A new list item'})
+        response = self.client.post('/', data={'item_text':'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/')
+        self.assertEqual(response['location'], '/')
 
     def test_displays_all_items(self):
         Item.objects.create(text='Item 1')
         Item.objects.create(text='Item 2')
 
-        response = self.client.get('/lists/')
-        
+        response = self.client.get('/')
+
         self.assertEqual(Item.objects.count(), 2)
         self.assertIn('Item 1', response.content.decode())
         self.assertIn('Item 2', response.content.decode())
@@ -51,5 +51,5 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, second_item.text)
 
     def test_only_saves_items_when_necessary(self):
-        self.client.get('/lists/')
+        self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
