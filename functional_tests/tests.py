6f0653843f2f1c8a2edff74377e5b788f42d23cb
Tawakalt <olaniyitaiwo95@yahoo.com>
2019-12-19 17:01:32 +0100

    add new url, view, templates and tests to display list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -93,7 +93,7 @@ class NewVisitorTest(LiveServerTestCase):
         # ​Francis visits the home page.  There is no sign of Edith's
         # list
         self.browser.get('%s%s' % (self.live_server_url, '/'))
-        page_text = self.browser.find_element_by_tag_name('body').header_text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
 
         # ​Francis starts a new list by entering a new item. He
@@ -109,9 +109,8 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # ​Again, there is no trace of Edith's list
-        page_text = self.browser.find_element_by_tag_name('body').header_text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
--- lists/tests.py
+++ lists/tests.py
@@ -18,17 +18,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text':'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_displays_all_items(self):
-        Item.objects.create(text='Item 1')
-        Item.objects.create(text='Item 2')
-
-        response = self.client.get('/')
-
-        self.assertEqual(Item.objects.count(), 2)
-        self.assertIn('Item 1', response.content.decode())
-        self.assertIn('Item 2', response.content.decode())
+        self.assertEqual(response['location'], '/lists/only-list/')
 
 
 class ItemModelTest(TestCase):
@@ -53,3 +43,21 @@ class ItemModelTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/only-list/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='Item 1')
+        Item.objects.create(text='Item 2')
+
+        response = self.client.get('/lists/only-list/')
+
+        self.assertEqual(Item.objects.count(), 2)
+        self.assertContains(response, 'Item 1')
+        self.assertContains(response, 'Item 2')
+   
