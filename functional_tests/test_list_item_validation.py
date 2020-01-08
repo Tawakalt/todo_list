27f82bb0bdc3ecdecbf5f4478e47845cee1ff1f5
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-08 17:55:57 +0100

    rename all item input ids and names NB: still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,7 +9,7 @@ class ItemValidationTest(FunctionalTest):
         # an empty list item. She hits Enter on the empty input box
         self.browser.get('%s%s' % (self.live_server_url, '/'))
 
-        inputbox = self.browser.find_element_by_id('new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
@@ -20,12 +20,12 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # She tries again with some text for the item, which now works
-        self.browser.find_element_by_id('new_item').send_keys('Buy Milk')
-        self.browser.find_element_by_id('new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Buy Milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Milk')
 
         # Perversely, she now decides to submit a second blank list item
-        self.browser.find_element_by_id('new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
         self.wait_for(lambda: self.assertEqual(
@@ -34,7 +34,7 @@ class ItemValidationTest(FunctionalTest):
         ))
 
         # And she can correct it by filling some text in
-        self.browser.find_element_by_id('new_item').send_keys('Make tea')
-        self.browser.find_element_by_id('new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Make tea')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Milk')
         self.wait_for_row_in_list_table('2: Make tea')
