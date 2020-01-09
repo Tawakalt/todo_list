34a7a8f8be9417a6a86d9b0e5b75dc6c2eadfc97
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-09 19:23:20 +0100

    use onload boilerplate and namespacing

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,5 @@
-const initialize = () => {
+window.Superlists = {}; // there are better ways of dealing with namespace in js
+window.Superlists.initialize = () => {
     $('input[name="text"]').on('keypress', () => {
         $('.has-error').hide();
     })
