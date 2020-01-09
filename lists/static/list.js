5acf43f63cbba7b3c6e480ff4a4b89c4a2ec512c
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-09 19:32:51 +0100

    extend functionality to clear error on input click

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,6 @@
 window.Superlists = {}; // there are better ways of dealing with namespace in js
 window.Superlists.initialize = () => {
-    $('input[name="text"]').on('keypress', () => {
+    $('input[name="text"]').on('keypress click', () => {
         $('.has-error').hide();
     })
 }
