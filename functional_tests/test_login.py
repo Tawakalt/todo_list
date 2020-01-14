3eae68ab5f11ee4e167571649b867256fdd33076
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-14 18:00:48 +0100

    fix typo

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -21,7 +21,7 @@ class LoginTest(FunctionalTest):
 
         # A message appears telling her an email has been sent to her email
         self.wait_for(lambda: self.assertIn(
-            'check your email',
+            'Check your email',
             self.browser.find_element_by_tag_name('body').text
         ))
 
