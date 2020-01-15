b5c7d52859a9a5750640dcd5bb885451644e8974
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-15 22:12:07 +0100

    edit value of DEBUG & ALLOWED_HOSTS in settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'todo-list-staging.herokuapp.com']
 
 
 # Application definition
