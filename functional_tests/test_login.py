af338db47dbf338548df2174fab71330c5d215e1
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-17 20:02:50 +0100

    use helper function in test

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -5,7 +5,6 @@ import os, poplib, re, time
 
 from .base import FunctionalTest
 
-TEST_EMAIL = 'olaniyitawakalt95test@gmail.com'
 SUBJECT = 'Your login link for Superlists'
 
 
@@ -45,8 +44,13 @@ class LoginTest(FunctionalTest):
         # Edith goes to awesome Superlists site
         # and notices a 'Log in' section in the navbar for the first time
         # It's telling her to enter her email address, so she does
+        if self.staging_server:
+            test_email = 'olaniyitaiwo95test@yahoo.com'
+        else:
+            test_email = 'olaniyitawakalt95test@gmail.com'
+
         self.browser.get('%s%s' % (self.live_server_url, '/'))
-        self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+        self.browser.find_element_by_name('email').send_keys(test_email)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         # A message appears telling her an email has been sent to her email
@@ -56,15 +60,13 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email and finds it
-email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_email, SUBJECT)
 
         # It has a url link in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'https://.+/.+$', body)
         if not url_search:
-            self.fail(f'Could not find url in email body:\n{email.body}')
+            self.fail(f'Could not find url in email body:\n{body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -72,10 +74,10 @@ email = mail.outbox[0]
         self.browser.get(url)
 
         # She is logged in!
-        self.wait_to_be_logged_in(email=TEST_EMAIL)
+        self.wait_to_be_logged_in(email=test_email)
 
         # Now she logs out
         self.browser.find_element_by_link_text('Log out').click()
 
         # She is logged out
-        self.wait_to_be_logged_out(email=TEST_EMAIL)
+        self.wait_to_be_logged_out(email=test_email)
