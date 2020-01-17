d1cb3f5cad82efb9ba45dd6af4df92526bd65efb
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-17 19:46:47 +0100

    create helper function that can retrieve real email from a real POP3 email server

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,6 @@
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
+import os, poplib, re, time
 
 
 from .base import FunctionalTest
@@ -10,6 +10,36 @@ SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.getenv('YAHOO_PASSWORD'))
+            while time.time() - start < 60:
+                #  get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting message', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
 
     def test_can_get_email_link_to_login(self):
         # Edith goes to awesome Superlists site
@@ -26,7 +56,7 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email and finds it
-        email = mail.outbox[0]
+email = mail.outbox[0]
         self.assertIn(TEST_EMAIL, email.to)
         self.assertEqual(email.subject, SUBJECT)
 
