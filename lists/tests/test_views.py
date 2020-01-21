3d83e342ddf2219cddc8f007ca46b2c04cfa00b6
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-01-21 16:26:09 +0100

    make new_list view  assign owner(failing)

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -154,6 +154,13 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        user = User.objects.create(email='ab@c.com')
+        self.client.force_login(user)
+        self.client.post('/lists/new', data={'text':'new item'})
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, user)
+
 
 class MyListsTest(TestCase):
 
