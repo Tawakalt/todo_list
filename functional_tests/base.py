c1b0f01ca10d4cb12df14426669519a9f1c16683
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-02-20 12:56:23 +0100

    move wait out of class to make it importable

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -9,6 +9,18 @@ from .server_tools import reset_database
 
 MAX_WAIT = 10
 
+def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
+
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -27,17 +39,6 @@ class FunctionalTest(StaticLiveServerTestCase):
         cls.browser.quit()
         super(FunctionalTest, cls).tearDownClass()
 
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
